c8840844aa7b92433e901af7c3f444d282854c6a
Bryan Salcedo <b.salcedomontt@gmail.com>
2025-08-14 18:13:42 -0400

    feat: add tf lxc workflow (#32)
    * chore: update swarm module name
    
    * ci: add tofu lxc workflow
    
    - uses bitwarden/sm-action to get secrets
    - uses self-hosted runner

--- proxmox/lxc/swarm.tf
+++ proxmox/lxc/swarm.tf
@@ -9,24 +9,24 @@ locals {
       description = "Docker Swarm node 02"
       ip          = "192.168.1.104"
       id          = 104
+    }
   }
 }
-}
 
-module "swarm-node-01" {
-    for_each           = local.swarm_nodes
-    source             = "git::https://github.com/Brsalcedom/tf-modules//proxmox/lxc?ref=v0.1.0"
-    node_name          = "prox"
-    vm_name            = each.key
-    vm_id              = each.value.id
-    os_template        = "debian-12-standard_12.7-1_amd64.tar.zst"
-    vm_cpu_cores       = 2
-    vm_memory          = 2048
-    vm_disk_size       = 15
-    vm_ipv4_address    = "${each.value.ip}/24"
-    vm_ipv4_gateway    = "192.168.1.1"
-    dns_server         = "192.168.1.150"
-    ssh_authorized_key = var.ssh_authorized_key
-    vm_description     = each.value.description
-    vm_tags            = ["terraform", "swarm", "docker"]
+module "swarm" {
+  for_each           = local.swarm_nodes
+  source             = "git::https://github.com/Brsalcedom/tf-modules//proxmox/lxc?ref=v0.1.0"
+  node_name          = "prox"
+  vm_name            = each.key
+  vm_id              = each.value.id
+  os_template        = "debian-12-standard_12.7-1_amd64.tar.zst"
+  vm_cpu_cores       = 2
+  vm_memory          = 2048
+  vm_disk_size       = 15
+  vm_ipv4_address    = "${each.value.ip}/24"
+  vm_ipv4_gateway    = "192.168.1.1"
+  dns_server         = "192.168.1.150"
+  ssh_authorized_key = var.ssh_authorized_key
+  vm_description     = each.value.description
+  vm_tags            = ["terraform", "swarm", "docker"]
 }

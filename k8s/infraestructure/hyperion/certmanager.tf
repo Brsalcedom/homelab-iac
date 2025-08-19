512748cafd7ede8f9e689f2ddd453248fde82fda
Bryan Salcedo <b.salcedomontt@gmail.com>
2025-08-18 22:57:31 -0400

    feat: k8s hyperion infra (#55)
    * feat: update tf boostrap & applications
    
    * fix: remove third party provider
    
    * feature: add hyperion infra deployment workflow
    
    * chore: remove unused env

--- k8s/infraestructure/hyperion/certmanager.tf
+++ k8s/infraestructure/hyperion/certmanager.tf
@@ -12,6 +12,9 @@ resource "helm_release" "cert_manager" {
   version          = local.cert_manager_chart_version
   create_namespace = false
 
+  wait    = true
+  timeout = 600
+
   set = [
     {
       name  = "crds.enabled"
@@ -26,48 +29,62 @@ resource "helm_release" "cert_manager" {
     }
   ]
   depends_on = [
-    helm_release.cilium, kubernetes_namespace.cert_manager
+    helm_release.cilium, kubernetes_namespace.cert_manager, time_sleep.after_cilium
   ]
 }
 
-resource "kubectl_manifest" "cloudflare_api_token_secret" {
-  yaml_body = <<YAML
-    apiVersion: v1
-    kind: Secret
-    metadata:
-      name: cloudflare-api-token-secret
-      namespace: "cert-manager"
-    type: Opaque
-    data:
-      api-token: ${base64encode(var.cloudflare_api_token)}
-YAML
+
+resource "null_resource" "cf_api_token_secret" {
+  triggers = {
+    namespace = kubernetes_namespace.cert_manager.metadata[0].name
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+kubectl -n cert-manager create secret generic cloudflare-api-token --from-literal=api-token="$CLOUDFLARE_API_TOKEN" --dry-run=client -o yaml | kubectl apply -f -
+EOT
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "kubectl -n cert-manager delete secret cloudflare-api-token --ignore-not-found"
+  }
+  depends_on = [kubernetes_namespace.cert_manager]
 }
 
 
-
-resource "kubectl_manifest" "clusterissuer" {
-  yaml_body = <<YAML
-apiVersion: cert-manager.io/v1
-kind: ClusterIssuer
-metadata:
-  name: ${local.cluster_issuer_name}
-spec:
-  acme:
-    email: ${var.cloudflare_email}
-    server: https://acme-v02.api.letsencrypt.org/directory
-    privateKeySecretRef:
-      name: cloudflare-clusterissuer-account-key
-    solvers:
-    - dns01:
-        cloudflare:
-          email: ${var.cloudflare_email}
-          apiTokenSecretRef:
-            name: cloudflare-api-token-secret
-            key: api-token
-YAML
+resource "kubernetes_manifest" "clusterissuer" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "ClusterIssuer"
+    "metadata" = {
+      "name" = "cloudflare-clusterissuer"
+    }
+    "spec" = {
+      "acme" = {
+        "email"  = var.cloudflare_email
+        "server" = "https://acme-v02.api.letsencrypt.org/directory"
+        "privateKeySecretRef" = {
+          "name" = "cloudflare-clusterissuer-account-key"
+        }
+        "solvers" = [
+          {
+            "dns01" = {
+              "cloudflare" = {
+                "apiTokenSecretRef" = {
+                  "name" = "cloudflare-api-token"
+                  "key"  = "api-token"
+                }
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
 
   depends_on = [
     helm_release.cert_manager,
-    kubectl_manifest.cloudflare_api_token_secret
+    null_resource.cf_api_token_secret
   ]
 }
--- k8s/infraestructure/hyperion/gateway.tf
+++ k8s/infraestructure/hyperion/gateway.tf
@@ -4,82 +4,130 @@ resource "kubernetes_namespace" "cilium_gateway" {
   }
 }
 
-resource "kubectl_manifest" "certificate" {
-  yaml_body  = <<YAML
-    apiVersion: cert-manager.io/v1
-    kind: Certificate
-    metadata:
-      name: wildcard-certificate
-      namespace: cilium-gateway
-    spec:
-      secretName: tls-cert
-      issuerRef:
-        name: ${local.cluster_issuer_name}
-        kind: ClusterIssuer
-      commonName: "*.${local.base_domain}"
-      dnsNames:
-        - "*.${local.base_domain}"
-YAML
-  depends_on = [kubernetes_namespace.cilium_gateway, helm_release.cilium, helm_release.cert_manager, kubectl_manifest.clusterissuer]
+resource "kubernetes_manifest" "certificate" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "Certificate"
+    "metadata" = {
+      "name"      = "wildcard-certificate"
+      "namespace" = "cilium-gateway"
+    }
+    "spec" = {
+      "secretName" = "tls-cert"
+      "issuerRef" = {
+        "name" = "cloudflare-clusterissuer"
+        "kind" = "ClusterIssuer"
+      }
+      "commonName" = "*.${local.base_domain}"
+      "dnsNames"   = ["*.${local.base_domain}"]
+    }
+  }
+
+  depends_on = [
+    kubernetes_namespace.cilium_gateway,
+    time_sleep.after_cilium,
+    helm_release.cert_manager,
+    kubernetes_manifest.clusterissuer,
+  ]
+}
+
+resource "kubernetes_manifest" "cilium_gateway" {
+  manifest = {
+    "apiVersion" = "gateway.networking.k8s.io/v1"
+    "kind"       = "Gateway"
+    "metadata" = {
+      "name"      = "cilium-gateway"
+      "namespace" = "cilium-gateway"
+    }
+    "spec" = {
+      "gatewayClassName" = "cilium"
+      "listeners" = [
+        {
+          "name"     = "https"
+          "port"     = 443
+          "protocol" = "HTTPS"
+          "hostname" = "*.${local.base_domain}"
+          "tls" = {
+            "mode" = "Terminate"
+            "certificateRefs" = [
+              {
+                "kind" = "Secret"
+                "name" = "tls-cert"
+              }
+            ]
+          }
+          "allowedRoutes" = {
+            "namespaces" = {
+              "from" = "All"
+            }
+          }
+        },
+        {
+          "name"     = "http"
+          "port"     = 80
+          "protocol" = "HTTP"
+          "hostname" = "*.${local.base_domain}"
+          "allowedRoutes" = {
+            "namespaces" = {
+              "from" = "All"
+            }
+          }
+        }
+      ]
+    }
+  }
+
+  depends_on = [
+    time_sleep.after_cilium,
+    kubernetes_namespace.cilium_gateway,
+    kubernetes_manifest.certificate,
+  ]
 }
 
 
-resource "kubectl_manifest" "cilium_gateway" {
-  yaml_body  = <<YAML
-apiVersion: gateway.networking.k8s.io/v1
-kind: Gateway
-metadata:
-  name: cilium-gateway
-  namespace: cilium-gateway
-spec:
-  gatewayClassName: cilium
-  listeners:
-    - name: https
-      port: 443
-      protocol: HTTPS
-      hostname: "*.${local.base_domain}"
-      tls:
-        mode: Terminate
-        certificateRefs:
-          - kind: Secret
-            name: tls-cert
-      allowedRoutes:
-        namespaces:
-          from: All
-    - name: http
-      port: 80
-      protocol: HTTP
-      hostname: "*.${local.base_domain}"
-      allowedRoutes:
-        namespaces:
-          from: All
-YAML
-  depends_on = [helm_release.cilium, kubernetes_namespace.cilium_gateway, kubectl_manifest.certificate]
-}
+resource "kubernetes_manifest" "cilium_redirect_httproute" {
+  manifest = {
+    "apiVersion" = "gateway.networking.k8s.io/v1"
+    "kind"       = "HTTPRoute"
+    "metadata" = {
+      "name"      = "redirect-to-https"
+      "namespace" = "cilium-gateway"
+    }
+    "spec" = {
+      "parentRefs" = [
+        {
+          "name"        = "cilium-gateway"
+          "sectionName" = "http"
+        }
+      ]
+      "hostnames" = ["*.${local.base_domain}"]
+      "rules" = [
+        {
+          "matches" = [
+            {
+              "path" = {
+                "type"  = "PathPrefix"
+                "value" = "/"
+              }
+            }
+          ]
+          "filters" = [
+            {
+              "type" = "RequestRedirect"
+              "requestRedirect" = {
+                "scheme"     = "https"
+                "statusCode" = 301
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
 
-resource "kubectl_manifest" "cilium_redirect_httproute" {
-  yaml_body  = <<YAML
-apiVersion: gateway.networking.k8s.io/v1
-kind: HTTPRoute
-metadata:
-  name: redirect-to-https
-  namespace: cilium-gateway
-spec:
-  parentRefs:
-    - name: cilium-gateway
-      sectionName: http
-  hostnames:
-    - "*.${local.base_domain}"
-  rules:
-    - matches:
-        - path:
-            type: PathPrefix
-            value: /
-      filters:
-        - type: RequestRedirect
-          requestRedirect:
-            scheme: https
-            statusCode: 301
-YAML
-  depends_on = [kubectl_manifest.cilium_gateway, kubernetes_namespace.cilium_gateway]
+  depends_on = [
+    time_sleep.after_cilium,
+    kubernetes_manifest.cilium_gateway,
+    kubernetes_namespace.cilium_gateway,
+  ]
 }

a4c01809497bc83118a43b9c2df3481d133b3217
Bryan Salcedo <b.salcedomontt@gmail.com>
2026-08-06 18:43:23 -0400

    feat(terraform/lxc): make Proxmox node name configurable via variable

--- terraform/proxmox/lxc/variables.tf
+++ terraform/proxmox/lxc/variables.tf
@@ -2,3 +2,9 @@ variable "ssh_authorized_keys" {
   description = "SSH authorized keys"
   type        = list(string)
 }
+
+variable "node_name" {
+  description = "Proxmox node name where containers will be deployed"
+  type        = string
+  default     = "prox"
+}
